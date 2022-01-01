7985ae1f2c211760aed5c1f433b4577489b146a1
Richard Medina <medinarichard996@gmail.com>
2021-12-31 23:10:52 -0800

    Fixing bundle

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return(
-      <div>hello</div>)
+      <div>hello world</div>)
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
