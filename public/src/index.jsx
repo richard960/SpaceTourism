2690c078c6e99faad3651d3fc195a271f5fa95d5
Richard Medina <medinarichard996@gmail.com>
2022-01-02 19:42:49 -0800

    Add icon

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  useEffect(() => {
+    document.body.id = 'home'
+  }, [])
     return(
       <div id='main'>
-
         <header>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+          <hr></hr>
           <nav>
             <a className='directory'><span>00</span> Home</a>
             <a className='transition'><span>01</span> Destination</a>
