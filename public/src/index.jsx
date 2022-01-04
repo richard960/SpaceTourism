1362e7a64343b6b40b3d3f0a2c0af746dea0448e
Richard Medina <medinarichard996@gmail.com>
2022-01-04 08:40:16 -0800

    Add media queries and css

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -4,13 +4,24 @@ import ReactDOM from 'react-dom';
 const App = () => {
 
   useEffect(() => {
-    document.body.id = 'home'
+    let elem = document.querySelector('#home-section');
+    let rect = elem.getBoundingClientRect();
+
+    if(rect.top - window.innerHeight <= 0) {
+      document.querySelector('.home-flex section').classList.add('fade-in');
+    }
+    window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop > rect.top) {
+        document.querySelector('.home-flex section').classList.add('fade-in');
+      }
+    })
+    document.body.id = 'home';
   }, []);
 
     return(
       <div id='main'>
         <header>
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
           <hr></hr>
           <nav>
             <a className='directory'><span>00</span> Home</a>
