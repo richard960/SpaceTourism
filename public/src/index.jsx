29b0380097b05cf3c8957f34471d53c41ace8c7b
Richard Medina <medinarichard996@gmail.com>
2022-01-05 04:59:19 -0800

    Added nav slide

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -23,7 +23,13 @@ const App = () => {
         <header>
         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fillRule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
           <hr></hr>
-          <nav>
+          <div id='nav-menu'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav id='nav-container'>
+          <svg id='close' xmlns="http://www.w3.org/2000/svg" width="20" height="21"><g fill="#D0D6F9" fill-rule="evenodd"><path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/><path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g></svg>
             <a className='directory'><span>00</span> Home</a>
             <a className='transition'><span>01</span> Destination</a>
             <a className='transition'><span>02</span> Crew</a>
